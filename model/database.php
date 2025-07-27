25dae339259b05aac60d5adc2c61840ab8fce6bc
JefferyG369 <jeff.ge@outlook.com.au>
2025-07-27 17:54:04 +1000

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,20 +1,23 @@
 <?php
+	//database connection details
+	$host = 'cfmysqldb.ccrghj6qojpg.us-east-1.rds.amazonaws.com';
+	$user = 'admin';
+	$password = 'Password123';
+	$database = 'awsexample';
 
-//database connection details
-$servername = "cfmysqldb.ccrghj6qojpg.us-east-1.rds.amazonaws.com";
-$username = "admin"
-$password = "Password";
-$database = "awsexample";
-
-//connect to the database
-try {
-	//connect to the database
-	$conn = new PDO(dsn: "mysql:host=$servername;dbname=$database", username: $username, password: $password);
-	//set the PDO error mode to exception
-	$conn->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
-}
-catch (PODException $e) {
-	echo "Connetion failed: " . $e->getMessage();
-}
-
+	//connect to database with a try/catch statement
+	//if the connection is not successful display the error message via database_error.php
+	//the PDOException class represents the error raised by PDO
+	//the PDO error is stored in the variable $e
+	//the PDO error is returned as a string via the getMessage() function
+	try
+	{
+		$conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+	}
+	catch(PDOException $e)
+	{
+		$error_message = $e->getMessage();
+		include('../view/database_error.php');
+		exit();
+	}
 ?>
